77af709f1840b658833ae892325d1c36113dd65e
charleserious <wujinjing25@gmail.com>
2018-02-23 16:26:38 +0800

    optimize App Shell

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
-    "extends": "google"
+    "env": {
+        "browser": true
+    },
+    "extends": "standard",
+    "rules": {
+        "indent": [2, 4],
+        'semi': [2, 'always'],
+    }
 };
