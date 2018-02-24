8f2e0a2a805c6a5b2f6eed29ca195150c711deb4
charleserious <wujinjing25@gmail.com>
2018-02-24 17:17:26 +0800

    "Cache then

--- src/Scripts/service-worker.js
+++ src/Scripts/service-worker.js
@@ -1,7 +1,8 @@
 (function () {
     'use strict';
 
-    var cacheName = 'pwaweather-staticfile';
+    var shellCacheName = 'pwaweather-appshell';
+    var dataCacheName = 'pwaweather-data';
 
     var filesToCache = [
         '../index.html',
@@ -25,7 +26,7 @@
     self.addEventListener('install', function (e) {
         console.log('[ServiceWorker] Install');
         e.waitUntil(
-            caches.open(cacheName).then(function (cache) {
+            caches.open(shellCacheName).then(function (cache) {
                 console.log('[ServiceWorker] Caching App Shell');
                 return cache.addAll(filesToCache);
             })
@@ -37,7 +38,7 @@
         e.waitUntil(
             caches.keys().then(function (keyList) {
                 return Promise.all(keyList.map(function (key) {
-                    if (key !== cacheName) {
+                    if (key !== shellCacheName) {
                         console.log('[ServiceWork] Removing old cache ', key);
                         return caches.delete(key);
                     }
@@ -50,10 +51,27 @@
 
     self.addEventListener('fetch', function (e) {
         console.log('[ServiceWorker] Fetch', e.request.url);
-        e.respondWith(
-            caches.match(e.request).then(function (response) {
-                return response || fetch(e.request);
-            })
-        );
+        var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+        // e.respondWith(
+        //     caches.match(e.request).then(function (response) {
+        //         return response || fetch(e.request);
+        //     })
+        // );
+        if (e.request.url.indexOf(dataUrl) > -1) {
+            e.respondWith(
+                caches.open(dataCacheName).then(function (cache) {
+                    return fetch(e.request).then(function (response) {
+                        cache.put(e.request.url, response.clone());
+                        return response;
+                    });
+                })
+            );
+        } else {
+            e.respondWith(
+                caches.match(e.request).then(function (response) {
+                    return response || fetch(e.request);
+                })
+            );
+        }
     });
 })();
